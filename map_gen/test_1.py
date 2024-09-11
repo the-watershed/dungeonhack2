a0946960448dfb2257bad3c2a9db2edf38afc241
tros-bellator <tejur7@gmail.com>
2024-09-11 06:31:16 -0500

    new mapgen

---
+++ map_gen/test_1.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.title("Map Generator")
+    root.geometry("800x600")
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
